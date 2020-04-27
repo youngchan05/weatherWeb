cac7ef4d6ad5c2b5fca7c8d26245219aa7211eb8
kim youngchan <kimynam1@naver.com>
2020-04-27 15:35:05 +0900

    수정

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 function weather() {
     const SEARCH_FORM = document.querySelector('.search-form');
     const SEARCH_INPUT  = SEARCH_FORM.querySelector('input');
+    const TEXT = document.querySelector('.text');
     const API_KEY = "59f55dcf0b2c981a05be562c29420cf2";
     const obj = {
         
@@ -18,13 +19,15 @@ function weather() {
             SEARCH_FORM.addEventListener('change' , inputText);
         },
         getWeather : function(city){
-            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
             ).then(function(response){
                 return response.json();
             })
             .then(function(json){
-                console.log(json);
-                if(json.cod === "404") console.log('error 404')
+                const cityName = json.name;
+                const cityTemp = json.main.temp;
+                TEXT.innerHTML = `${cityName} ${cityTemp}`
+                if(json.cod === "404") console.log('error 404') ;
             })
         },
      }
